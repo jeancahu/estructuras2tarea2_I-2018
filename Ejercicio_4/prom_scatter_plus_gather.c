3e5b43f6a93291cf9ae0c96fe5b8544d471fce49
Jeancarlo Hidalgo <jeancahu@gmail.com>
2018-06-30 17:51:45 -0600

    add: print local prom, example scatter and gather

--- Ejercicio_4/prom_scatter_plus_gather.c
+++ Ejercicio_4/prom_scatter_plus_gather.c
@@ -5,13 +5,13 @@
 
 void fillVector(int filas, int *mat)
 {
-    srand(time(NULL));
-    int x;
-    unsigned int y = 0;
-    y = y - 1;
-    y = y / filas;
+  srand(time(NULL));
+  int x;
+  unsigned int y = 0;
+  y = y - 1;
+  y = y / filas;
 
-    for(x = 0; x < filas; x++) 
+  for(x = 0; x < filas; x++) 
     {
       mat[x] = rand() % y;
     }
@@ -20,33 +20,57 @@ void fillVector(int filas, int *mat)
 
 int main(int argc, char ** argv)
 {
-    
-    int i, j, k, rank, size;//tag = 99, sum = 0;
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-        
-    
-    // Root process
-    if( rank == 0) {
-        //Generación aleatoria de matriz
-      int tam;
-      printf("Inserte numero de elementos: ");
-      fflush(stdout);
-      scanf("%d", &tam);
 
-      int* vector = malloc(tam * sizeof(int));
-      fillVector(tam, vector);
+  int root = 0;
+  int i, j, k, rank, size;
 
-      for (i = 0; i < tam; i++) {
-	printf("%d\n", vector[i]);
-      }
-    } else {
-      printf("Hola\n");
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  // Cantidad de datos
+  int tam;
+  // Cantidad de datos en procesos hijos
+  int local_tam;
+  
+  // Total del promedio por proceso
+  int total = 0;
+  
+  // Pointer to vector
+  int* vector;
+  int* sub_vector;
+    
+  // Root process
+  if( rank == root ) {
+    //Generación aleatoria de matriz
+    printf("Inserte numero de elementos: ");
+    fflush(stdout);
+    scanf("%d", &tam);
+
+    vector = malloc(tam * sizeof(int));
+    fillVector(tam, vector);
+    
+    for (i = 0; i < tam; i++) {
+      printf("%d\n", vector[i]);
     }
+  }
 
-    MPI_Finalize();
+  sub_vector = malloc( (tam/size) * sizeof(int));
+  MPI_Scatter( &tam, 1, MPI_INT, &local_tam, 1, MPI_INT, root, MPI_COMM_WORLD);
+  MPI_Scatter(vector, tam/size, MPI_INT, sub_vector, tam/size, MPI_INT, root, MPI_COMM_WORLD);
 
-    return 0;    
+  
+  
+  for (i = 0; i < local_tam/size; i++) {
+    printf("%d %d\n", rank, sub_vector[i]);
+    total = total + sub_vector[i];
+  }
+
+  total = total / local_tam;
+
+  printf("%d\n", total);
+
+  MPI_Finalize();
+
+  return 0;    
 }
-
