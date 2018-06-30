bdc6a555f3bdeca1cd120bdc68ad980f5cf7c681
Caro <carotq777@gmail.com>
2018-06-30 11:24:59 -0600

    add: Generador de primos secuencial

---
+++ Prime_sec_algorithm.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define TOP_COUNT_VALUE=500000;
+
+int prime(int testValue)
+{
+    int isPrime = 1;
+    for(int i=3; (i*i)<=testValue;i+=2)
+    {
+        if(testValue%i==0)
+        {
+            isPrime = 0;
+        }   
+    }  
+    return isPrime;
+}  
+
+int main()
+{
+    int counter=1;//Ya tenemos a 2
+    int testValue=3;
+    while (counter<TOP_COUNT_VALUE)
+    {
+        if(prime(testValue)==1)
+        {
+            counter++;
+            printf("%d\n",testValue);
+        }
+        testValue+=2;    
+    }
+}
+
+  
