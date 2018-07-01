1f2b5e980d8194f6958d9954ac5f8577c0548be3
Caro <carotq777@gmail.com>
2018-06-30 22:46:10 -0600

    Arreglo de parámetros faltantes

--- EOpc3.c
+++ EOpc3.c
@@ -3,15 +3,14 @@
 
 int main(int argc, char ** argv)
 {
-    int rank, size, token;
-    
-    
+    int rank, size, token, next;
     MPI_Status status;
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     
-
+    token = 5;
+    next = (rank+1)%size;
     if(rank == 0)
     {
         printf(" Ejercicio 1.3: p0-p1-p2-p3-p0. \n");
@@ -32,4 +31,3 @@ int main(int argc, char ** argv)
 
 
 }
- 
