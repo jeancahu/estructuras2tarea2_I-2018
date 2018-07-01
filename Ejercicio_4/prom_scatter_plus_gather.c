f757b7b4b6d44e940098a0ade2a81880a054a27a
Jeancarlo Hidalgo <jeancahu@gmail.com>
2018-06-30 20:20:51 -0600

    add: prom scatter plus gather ready for odd vectors too

--- Ejercicio_4/prom_scatter_plus_gather.c
+++ Ejercicio_4/prom_scatter_plus_gather.c
@@ -9,7 +9,7 @@ void fillVector(int filas, int *mat)
   int x;
   unsigned int y = 0;
   y = y - 1;
-  y = y / filas;
+  y = y / (filas * 2);
 
   for(x = 0; x < filas; x++) 
     {
@@ -29,68 +29,109 @@ int main(int argc, char ** argv)
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   // Cantidad de datos
-  int tam;
-  
+  int tam = 0;
+  int local_tam;
+
+  // Elementos sobrantes
+  // Se almacenan en el total del proceso root
+
   // Total del promedio por proceso
   int total = 0;
   int* total_buf;
-  total_buf = malloc( size * sizeof(int));
+
   // Pointer to vector
   int* vector;
-    
+
   // Root process
   if( rank == root ) {
     //Generación aleatoria de matriz
-    printf("Inserte numero de elementos: ");
+    printf("\nInserte numero de elementos: ");
     fflush(stdout);
     scanf("%d", &tam);
 
-    vector = malloc( tam * sizeof(int));
-    fillVector(tam, vector);
-
-    printf("\n\nElementos del vector:\n\n");
-    for (i = 0; i < tam; i++) {
-      printf("%d\n", vector[i]);
+    if ( tam < size ) {
+      printf("\nError: El valor es demasiado pequeño\n");
+      printf("Valor mínimo permitido es %d\n", size);
+      tam = 0;
     }
-    printf("\n\n");
-  } else {
-    vector = malloc( (tam/size) * sizeof(int));
+    else if ( tam > 200000 ) {
+      printf("\nError: El valor es demasiado grande\n");
+      printf("Valor máximo permitido es 200000\n");
+      tam = 0;
+    } else {
+      // printf("\n%d\n", tam%size);
+      total_buf = malloc( size * sizeof(int));
+      vector = malloc( tam * sizeof(int));
+      fillVector(tam, vector);
+
+      printf("\nElementos del vector:\n\n");
+      for (i = 0; i < tam; i++) {
+	printf("%d\n", vector[i]);
+      }
+
+      // printf("\n%d\n", vector);
+      // Operar elementos extras:
+      for (i = 0; i < tam%size; i++){
+	total = total + vector[i];
+      }
+      vector = &vector[(tam%size)];
+      // tam = tam - (tam%size);
+      // printf("\n%d\n", vector);
+    }
   }
-  
+
   MPI_Bcast( &tam, 1, MPI_INT, root, MPI_COMM_WORLD);
 
+  if ( tam ) {
+    if ( rank != root ) {
+      vector = malloc( (tam/size) * sizeof(int));
+    }
+  } else {
+    MPI_Finalize();
+    return 0;
+  }
+
   // printf("%d %d %d\n", total, tam, size);
-  
-  MPI_Scatter(vector, tam/size, MPI_INT,
-	      vector, tam/size, MPI_INT, root,
+
+  // Tamaños de los buffers locales
+  local_tam = (tam/size);
+
+  // Se divide el vector de datos para otorgar un segmento a cada
+  // proceso hijo y un arreglo al proceso matriz
+  MPI_Scatter(vector, local_tam, MPI_INT,
+	      vector, local_tam, MPI_INT, root,
 	      MPI_COMM_WORLD);
-  
-  for (i = 0; i < tam/size; i++) {
+
+  for (i = 0; i < local_tam; i++) {
     // printf("%d %d\n", rank, vector[i]);
     total = total + vector[i];
   }
 
-  // printf("%d %d %d\n", total, tam, size);
-  total = total / ( tam / size );
+  if ( rank == root ) {
+    // Se divide tomando en cuenta los elementos sobrantes
+    total = total / ( local_tam + tam%size ) ;
+  } else {
+    // Se divide tomando en cuenta los elementos locales
+    total = total / local_tam;
+  }
 
-  printf("Total individual: %d\n", total);
-  
+  // printf("%d %d %d\n", total, tam, size);
+
   MPI_Gather( &total, 1, MPI_INT,
   	      total_buf, 1, MPI_INT, root,
   	      MPI_COMM_WORLD);
 
   if ( rank == root ) {
-    printf("\n\n");
     total = 0;
     for (i = 0; i < size; i++) {
-      //printf("%d\n", total_buf[i]);
+      // printf("TODO: %d\n", total_buf[i]);
       total = total + total_buf[i];  
     }
+    total = total / size;
 
-    printf("El promedio es: %d\n", total);
-    
+    printf("\nEl promedio es: %d\n\n", total);
+
   }
   MPI_Finalize();
-
   return 0;    
 }
